57f4cc3b5cdad51cdce1a6699b650954f5a0fa92
Robyn Noble <robynhurd09@gmail.com>
2021-03-18 07:21:18 -0500

    almost done

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,23 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket=[]
+function addItem( item ){
+  basket.push ('newItem');
+  basket.push ('apples', 'bananas', 'strawberries');
+  return true;
+}
+console.log('The item was added to the basket:', addItem());
+
+function listItems(){
+    for(x=0; x=basket.length; x++);
+}
+for (let item of basket){
+  console.log('The items in the basket are: ', item)
+}
+
+function empty( empty ){
+  basket.length = 0;
+  return basket;
+}
+console.log('The items in the basket now are: ', basket);
